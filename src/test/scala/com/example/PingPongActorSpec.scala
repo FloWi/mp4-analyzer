59d94e09e0af3c8cb576d0ab5924f6c689cf42a0
Florian Witteler <florian_witteler@gmx.de>
2015-04-13 21:57:27 +0200

    working solution

--- src/test/scala/com/example/PingPongActorSpec.scala
+++ src/test/scala/com/example/PingPongActorSpec.scala
@@ -47,15 +47,38 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
 
 
       case class Coordinate(x: Int, y: Int)
-      val coordinates = for (x <- 12.to(67);
-                             y <- 187.to(642))
-        yield Coordinate(x, y)
+      val fromLeftCoordinates = (for (x <- 62.to(75);
+                                      y <- 183.to(638))
+        yield Coordinate(x, y)).toList
 
-      var referenceColor: Option[Int] = None
+      val fromRightCoordinates = (for (x <- 462.to(475);
+                                       y <- 183.to(638))
+        yield Coordinate(x, y)).toList
+
+      val fromTopCoordinates = (for (x <- 98.to(440);
+                                     y <- 143.to(154))
+        yield Coordinate(x, y)).toList
+
+      val fromBottomCoordinates = (for (x <- 98.to(440);
+                                        y <- 667.to(680))
+        yield Coordinate(x, y)).toList
+
+      case class CoordinatesAndDirection(coordinates: List[Coordinate], direction: String)
+
+      val coordinates = List(
+        CoordinatesAndDirection(fromLeftCoordinates, "left"),
+        CoordinatesAndDirection(fromRightCoordinates, "right"),
+        CoordinatesAndDirection(fromTopCoordinates, "top"),
+        CoordinatesAndDirection(fromBottomCoordinates, "bottom")
+      )
+
+
+      var referenceColor: Option[Color] = None
 
       var lastDiffedImage: Option[BufferedImage] = None
-      var lastMatchedFrame = 0
+      var lastFrameWithColorChange: Option[FrameNumberChangeDetected] = None
 
+      case class FrameNumberChangeDetected(frameNumber: Int, direction: String)
 
       1.to(numberOfFrames).foreach { frameNumber =>
 
@@ -63,33 +86,42 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
 
         bufferedImage.foreach { image =>
 
+          //check, if this image is n frames after the matchedFrame
+          if (lastFrameWithColorChange.isDefined && frameNumber >= lastFrameWithColorChange.get.frameNumber + 15) {
+            ImageIO.write(image, "png", new File(s"/home/flwi/Pictures/threes-capture/${frameNumber.formatted(formatString)}-newBoard-from-${lastFrameWithColorChange.get.direction}.png"))
+            lastFrameWithColorChange = None
+          }
+
           if (lastDiffedImage.isEmpty) {
-            println("set reference image")
+            println(s"#$frameNumber set reference image")
             lastDiffedImage = Some(image)
 
             //store reference color if not already set
             if (referenceColor.isEmpty) {
-              referenceColor = Some(image.getRGB(12, 187))
+              referenceColor = Some(new Color(image.getRGB(12, 187)))
             }
           } else {
 
+            //check for change
             if (!bufferedImagesEqual(lastDiffedImage.get, image)) {
-              println("found diffing image, analyzing ...")
+              println(s"#$frameNumber found diffing image, analyzing. further...")
               lastDiffedImage = Some(image)
 
-              //check, if this image is n frames after the matchedFrame
-              if (frameNumber == lastMatchedFrame + 3) {
-                ImageIO.write(image, "png", new File(s"/home/flwi/Pictures/threes-capture/${frameNumber.formatted(formatString)}-newBoard.png"))
-              }
+              coordinates.foreach { case CoordinatesAndDirection(coords, direction) =>
 
-              val colors = coordinates.map(coord => image.getRGB(coord.x, coord.y))
-              val allSameAsReference = colors.forall(c => c == referenceColor.get)
+                val colors = coords.map(coord => new Color(image.getRGB(coord.x, coord.y)))
+                val numberOfPixelsChanged: Int = colors.count(c => calcColorDistance(c, referenceColor.get) > 100)
 
-              if (!allSameAsReference) {
-                println(s"found match in $frameNumber")
-                println("colors: " + colors.groupBy(c => c).map(tup => s"${tup._1}: ${tup._2.size}x"))
-                lastMatchedFrame = frameNumber
-                ImageIO.write(image, "png", new File(s"/home/flwi/Pictures/threes-capture/${frameNumber.formatted(formatString)}-match.png"))
+                val significantChangeDetected = numberOfPixelsChanged > coords.size * 0.10
+
+                if (significantChangeDetected) {
+                  lastFrameWithColorChange = Some(FrameNumberChangeDetected(frameNumber, direction))
+
+                  println(s"#$frameNumber found match in $frameNumber")
+                  println(s"#$frameNumber colors: " + colors.groupBy(c => c).map(tup => s"${tup._1}: ${tup._2.size}x"))
+                  ImageIO.write(image, "png", new File(s"/home/flwi/Pictures/threes-capture/${frameNumber.formatted(formatString)}-match-from-$direction.png"))
+
+                }
               }
             }
           }
@@ -100,6 +132,17 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
     }
   }
 
+  def calcColorDistance(c1: Color, c2: Color): Double = {
+    val rmean: Double = (c1.getRed + c2.getRed) / 2
+    val r: Int = c1.getRed - c2.getRed
+    val g: Int = c1.getGreen - c2.getGreen
+    val b: Int = c1.getBlue - c2.getBlue
+    val weightR: Double = 2 + rmean / 256
+    val weightG: Double = 4.0
+    val weightB: Double = 2 + (255 - rmean) / 256
+    math.sqrt(weightR * r * r + weightG * g * g + weightB * b * b)
+  }
+
   def bufferedImagesEqual(img1: BufferedImage, img2: BufferedImage): Boolean = {
 
     def getColors(img: BufferedImage) = for (x <- 0.until(img.getWidth);
@@ -107,16 +150,6 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
       yield new Color(img.getRGB(x, y))
 
 
-    def calcColorDistance(c1: Color, c2: Color): Double = {
-      val rmean: Double = (c1.getRed + c2.getRed) / 2
-      val r: Int = c1.getRed - c2.getRed
-      val g: Int = c1.getGreen - c2.getGreen
-      val b: Int = c1.getBlue - c2.getBlue
-      val weightR: Double = 2 + rmean / 256
-      val weightG: Double = 4.0
-      val weightB: Double = 2 + (255 - rmean) / 256
-      math.sqrt(weightR * r * r + weightG * g * g + weightB * b * b)
-    }
 
     if (img1.getWidth != img2.getWidth || img1.getHeight != img2.getHeight) {
       false
@@ -130,7 +163,7 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
         //.map{ case (c1, c2) => c1.getRGB-c2.getRGB }
         .zipWithIndex
         .map { case ((c1, c2), index) => ((c1, c2, calcColorDistance(c1, c2)), index) }
-        .filter { case ((_, _, diff), _) => diff > 100 }.toList
+        .filter { case ((_, _, diff), _) => diff > 120 }.toList
 
       if (diffingPixels.size < 100) {
 
