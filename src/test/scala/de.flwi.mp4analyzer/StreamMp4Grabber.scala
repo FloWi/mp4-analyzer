16d3b5f2c80795b2fda1dceddab8c066566800e0
Florian Witteler <florian_witteler@gmx.de>
2015-04-14 23:09:28 +0200

    akka-streams first steps

--- src/test/scala/de.flwi.mp4analyzer/StreamMp4Grabber.scala
+++ src/test/scala/de.flwi.mp4analyzer/StreamMp4Grabber.scala
@@ -1,5 +1,30 @@
 package de.flwi.mp4analyzer
 
+import java.awt.image.BufferedImage
+
+import akka.actor.ActorSystem
+import akka.stream.{Outlet, ActorFlowMaterializer}
+import akka.stream.scaladsl.{FlowGraph, Source}
+
 class StreamMp4Grabber {
 
+  implicit val system = ActorSystem("reactive-frames")
+  implicit val materzializer = ActorFlowMaterializer()
+
+
+  val frames: Source[Int, Unit] = Source() { implicit b =>
+
+
+    import FlowGraph.Implicits._
+
+    val frames = Source(() => Iterator.from(1))
+
+    val outlet: Outlet[Int] = b.add(frames)
+
+    outlet
+
+  }
+
 }
+
+
